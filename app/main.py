40a78c0295241551d0b42c3d9970b347fd774a0e
manggong2 <yejin05123@naver.com>
2024-04-12 15:35:22 +0900

    fastapi

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
+from typing import Union
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
-def root():
+def read_root():
     return { "message": "Hello Yejin!" }
 
-@app.get("/home")
-def home():
-    return { "message": "Home!" }
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return { "item_id": item_id, "q":q }

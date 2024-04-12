7057d3b2b1d9dc6029be28436c2aa8fb8792a98b
manggong2 <yejin05123@naver.com>
2024-04-12 16:44:53 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,21 +3,27 @@ from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-app = FastAPI()
-
 class Item(BaseModel):
     name: str
+    description: Union[str, None] = None
     price: float
-    is_offer: Union[bool, None] = None
 
 @app.get("/")
-def read_root():
-    return { "message": "Hello Yejin!" }
+async def read_root():
+    return "This is root path from MyAPI"
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return { "item_id": item_id, "q":q }
+async def read_item(item_id: int, q: Union[str,  None] = None):
+    return {"item_id": item_id, "q": q}
 
-@app.post("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return { "item_name": item.name, "item_id": item_id }
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    result = {"item_id": item_id, **item.dict()}
+
+@app.delete("/items/{item_id}")
+def delete_item(item_id: int):
+    return {"deleted": item_id}

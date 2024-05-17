bae42b535734b160760afa5b9b97b1a518a7c395
manggong2 <yejin05123@naver.com>
2024-05-17 17:10:19 +0900

    xml->json

--- app/main.py
+++ app/main.py
@@ -1,27 +1,20 @@
 from fastapi import FastAPI
 import requests
-import xml.etree.ElementTree as ET
 
 app = FastAPI()
 
 @app.get("/")
 def root():
-    URL = "https://openapi.naver.com/v1/search/book.xml?query=%EC%A3%BC%EC%8B%9D&display=10&start=1"
+    URL = "https://openapi.naver.com/v1/search/book.json?query=%EC%A3%BC%EC%8B%9D&display=10&start=1"
     headers = {
         "X-Naver-Client-Id": "mqw55g68zcYsPz010T6X",
         "X-Naver-Client-Secret": "hsMn6N5zrC"
     }
 
     res = requests.get(URL, headers=headers)
+    print(res.status_code)
     if res.status_code == 200:
-        # XML 파싱
-        root = ET.fromstring(res.content)
-        isbn_list = []
-        for item in root.findall(".//item"):
-            isbn = item.find("isbn")
-            if isbn is not None:
-                isbn_list.append(isbn.text)
-        return {"isbn_list": isbn_list}
+        return res.text
     else:
         return {"error": "Request failed with status code " + str(res.status_code)}
 

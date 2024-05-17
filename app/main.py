f5a7358bbdcea616867396b1a4c447663c1ed3dd
manggong2 <yejin05123@naver.com>
2024-05-17 16:55:53 +0900

    naver_isbn

--- app/main.py
+++ app/main.py
@@ -1,17 +1,30 @@
 from fastapi import FastAPI
-
 import requests
+import xml.etree.ElementTree as ET
 
 app = FastAPI()
 
 @app.get("/")
 def root():
-    URL = "https://bigdata.kepco.co.kr/openapi/v1/powerUsage/industryType.do?year=2023&month=12&apiKey=34cuFl36WbAEE5JClbnupw2A3oS7qW7f1i15II5D&returnType=json"
+    URL = "https://openapi.naver.com/v1/search/book.xml?query=%EC%A3%BC%EC%8B%9D&display=10&start=1"
+    headers = {
+        "X-Naver-Client-Id": "mqw55g68zcYsPz010T6X",
+        "X-Naver-Client-Secret": "hsMn6N5zrC"
+    }
 
-    contents = requests.get(URL).text
-
-    return { "message": contents }
+    res = requests.get(URL, headers=headers)
+    if res.status_code == 200:
+        # XML 파싱
+        root = ET.fromstring(res.content)
+        isbn_list = []
+        for item in root.findall(".//item"):
+            isbn = item.find("isbn")
+            if isbn is not None:
+                isbn_list.append(isbn.text)
+        return {"isbn_list": isbn_list}
+    else:
+        return {"error": "Request failed with status code " + str(res.status_code)}
 
 @app.get("/home")
 def home():
-    return { "message": "Home!" } 
+    return {"message": "Home!"}
